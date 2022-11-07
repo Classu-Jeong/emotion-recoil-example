62a9b8a563af187126d3c7d39c81f5b3939099d5
jeong <jeong@classu.co.kr>
2022-11-07 18:15:04 +0900

    ing: error still exists todo list 구현 중이고, 아직 미완성, 저장한 링크 보고 한 번 다시 만들기

--- todo-list/pages/todo/index.tsx
+++ todo-list/pages/todo/index.tsx
@@ -1,32 +1,25 @@
 import Link from 'next/link'
 import { FormEvent } from 'react'
 import { useRecoilState } from 'recoil'
-import userState from 'src/recoil/user'
+import { todoListState } from 'src/recoil/todo'
 
-export default function Home() {
-    const [user, setUser] = useRecoilState(userState)
+export default function Todo() {
+    const [todoList, setTodoList] = useRecoilState(todoListState)
+    const index = todoList.findIndex((listItem) => listItem === 0)
 
-    const handleChangeUser = (event: FormEvent<HTMLFormElement>) => {
-        const { value } = event.target as HTMLInputElement
-        if (value) setUser(value)
-    }
-
-    const handleSubmitUser = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-    }
     return (
         <div>
             <div>
                 <h1>Todo</h1>
             </div>
             <div>
-                <span>todo : {user}</span>
+                <span>todo : {todoList}</span>
             </div>
             <div>
-                <form onSubmit={(e) => handleChangeUser(e)}>
+                <form>
                     <input
                         placeholder="유저 입력하기"
-                        value={user}
+                        value={todoList}
                         type="text"
                     />
                     <button type="submit">확인하기</button>
